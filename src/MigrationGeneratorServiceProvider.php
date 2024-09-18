5ba15fb701894d12588c6a615b2cebdd22fe7a3b
fouadsalkini <fouad@sync.com.lb>
2024-09-18 17:53:51 +0300

    initialization

---
+++ src/MigrationGeneratorServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FDS\MigrationGeneratorServiceProvider;
+
+use Illuminate\Support\ServiceProvider;
+
+class MigrationGeneratorServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Publishing config, migrations, or views.
+        $this->publishes([
+            __DIR__.'/src/config/migration-generator.php' => config_path('migration-generator.php'),
+        ]);
+    }
+
+    public function register()
+    {
+        // Merge the config if necessary
+        $this->mergeConfigFrom(
+            __DIR__.'/src/config/migration-generator.php', 'migration-generator'
+        );
+    }
+}
